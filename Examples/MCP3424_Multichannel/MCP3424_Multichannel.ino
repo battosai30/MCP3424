6e7dbeb52f2527a6f991b3ab535c434731ba508a
battosai30 <batto@hotmail.fr>
2021-08-27 18:43:26 +0200

    Update MCP3424_Multichannel.ino

--- Examples/MCP3424_Multichannel/MCP3424_Multichannel.ino
+++ Examples/MCP3424_Multichannel/MCP3424_Multichannel.ino
@@ -1,4 +1,4 @@
-/* MCP 3424 version 1.3 example sketch Multichannel
+/* MCP 3424 version 1.9 example sketch Multichannel
 Written by B@tto 
 Contact : batto@hotmail.fr
 
@@ -24,9 +24,9 @@ void loop(){
   Serial.println(""); 
   Serial.println("12 bits"); 
   
-  for(int i=1;i<=4;i++){
+  for(byte i=1;i<=4;i++){
 
-      MCP.setConfiguration(i,RESOLUTION_12_BITS,CONTINUOUS_MODE,PGA_X8); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
+    MCP.setConfiguration(i,RESOLUTION_12_BITS,CONTINUOUS_MODE,PGA_X8); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
 
     Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
 
@@ -41,51 +41,51 @@ void loop(){
   Serial.println(""); 
   Serial.println("14 bits"); 
   
-    for(int i=1;i<=4;i++){
+    for(byte i=1;i<=4;i++){
 
       MCP.setConfiguration(i,RESOLUTION_14_BITS,CONTINUOUS_MODE,PGA_X8); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
 
-    Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
+      Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
 
-    Serial.print("Channel "); // print results
-    Serial.print(i);
-    Serial.print(" : ");  
-    Serial.print(Voltage[i-1]);
-    Serial.println(" microVolt");
+      Serial.print("Channel "); // print results
+      Serial.print(i);
+      Serial.print(" : ");  
+      Serial.print(Voltage[i-1]);
+      Serial.println(" microVolt");
 
   }
 
   Serial.println(""); 
   Serial.println("16 bits"); 
   
-    for(int i=1;i<=4;i++){
+    for(byte i=1;i<=4;i++){
 
-    MCP.setConfiguration(i,RESOLUTION_16_BITS,CONTINUOUS_MODE,PGA_X8); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
+      MCP.setConfiguration(i,RESOLUTION_16_BITS,CONTINUOUS_MODE,PGA_X8); // MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
 
-    Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
+      Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
 
-    Serial.print("Channel "); // print results
-    Serial.print(i);
-    Serial.print(" : ");  
-    Serial.print(Voltage[i-1]);
-    Serial.println(" microVolt");
+      Serial.print("Channel "); // print results
+      Serial.print(i);
+      Serial.print(" : ");  
+      Serial.print(Voltage[i-1]);
+      Serial.println(" microVolt");
 
   }
 
   Serial.println(""); 
   Serial.println("18 bits"); 
   
-    for(int i=1;i<=4;i++){
+    for(byte i=1;i<=4;i++){
 
-    MCP.setConfiguration(i,RESOLUTION_18_BITS,CONTINUOUS_MODE,PGA_X8);// MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
+      MCP.setConfiguration(i,RESOLUTION_18_BITS,CONTINUOUS_MODE,PGA_X8);// MCP3424 is configured to channel i with 18 bits resolution, continous mode and gain defined to 8 
 
-    Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
+      Voltage[i-1]=MCP.measure(); // Measure is stocked in array Voltage, note that the library will wait for a completed conversion that takes around 200 ms@18bits
 
-    Serial.print("Channel "); // print results
-    Serial.print(i);
-    Serial.print(" : ");  
-    Serial.print(Voltage[i-1]);
-    Serial.println(" microVolt");
+      Serial.print("Channel "); // print results
+      Serial.print(i);
+      Serial.print(" : ");  
+      Serial.print(Voltage[i-1]);
+      Serial.println(" microVolt");
 
   }
 }
